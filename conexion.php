46914451a11f1f1980d4f1485b4dc9f292863b5c
VanessaRmrz <vane11itzarrahi@gmail.com>
2025-07-15 13:47:20 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,14 @@
 <?php
-$host = 'localhost';
-$db = 'cocinapower';
-$user = 'postgres';
-$pass = 'V02032003';
+$host = 'dpg-d1rask15pdvs73fa0i4g-a.oregon-postgres.render.com';
+$db   = 'cocinapower';
+$user = 'vanessa';
+$pass = '41ghfUoaV4UTZsp807mz4OZPiJ7BXyOx';
 $port = '5432';
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error al conectar con la base de datos: " . $e->getMessage());
+    die("Error al conectar con la base de datos en Render: " . $e->getMessage());
 }
 ?>
